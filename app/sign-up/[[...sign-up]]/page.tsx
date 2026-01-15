2df5a7185e1e5c45e27b8b716d59d5dcaaba66f6
arfin-web <arfinpriom12@gmail.com>
2026-01-15 14:18:39 +0600

    initial auth setup by using clerk

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,15 @@
+import { Footer } from '@/components/landing-page/Footer'
+import { Navbar } from '@/components/landing-page/Navbar'
+import { SignUp } from '@clerk/nextjs'
+
+export default function Page() {
+    return (
+        <>
+            <Navbar />
+            <div className="flex min-h-screen items-center justify-center">
+                <SignUp />
+            </div>
+            <Footer />
+        </>
+    )
+}
--- components/landing-page/Navbar.tsx
+++ components/landing-page/Navbar.tsx
@@ -2,8 +2,11 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { AiBrain03Icon } from "@hugeicons/core-free-icons";
+import { UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server'
 
-export function Navbar() {
+export async function Navbar() {
+    const user = await currentUser();
     return (
         <header className="fixed top-0 left-0 right-0 z-50 border-b border-border/40 bg-background/80 backdrop-blur-md">
             <div className="container mx-auto flex h-16 items-center justify-between px-4 md:px-6">
@@ -26,13 +29,17 @@ export function Navbar() {
                         Testimonials
                     </Link>
                 </nav>
-                <div className="flex items-center gap-4">
-                    <Button variant="ghost" size="sm" className="hidden sm:inline-flex">
-                        Log in
-                    </Button>
-                    <Button size="sm" className="rounded-full px-6">
-                        Get Started
-                    </Button>
+                <div>
+                    {
+                        user ? <UserButton /> : <div className="flex items-center gap-4">
+                            <Button variant="ghost" size="sm" className="hidden sm:inline-flex">
+                                <Link href="/signin">Login</Link>
+                            </Button>
+                            <Button size="sm" className="rounded-full px-6">
+                                <Link href="/signup">Get Started</Link>
+                            </Button>
+                        </div>
+                    }
                 </div>
             </div>
         </header>
